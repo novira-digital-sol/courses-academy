229c646dc88335f0d48c6b382c5064bcce81c6e3
nadaalaa467 <n192004a@gmail.com>
2026-07-25 23:16:23 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { Toaster } from "react-hot-toast";
 
-import './App.css'
+import HomeLayout from "./components/layout/HomeLayout";
+import Landing from "./pages/Landing";
+import AllBlogsPage from "./components/landing/AllBlogsPage";
+
+import BlogPostPage from "./components/landing/Blogpostpage";
+
 
 function App() {
+  const { user } = useContext(AuthContext);
 
   return (
     <>
-     <h2>hello</h2>
+      <Toaster
+        position="top-left"
+        reverseOrder={false}
+        toastOptions={{ style: { direction: "ltr" } }}
+      />
+
+      <Routes>
+        {/* Landing */}
+        <Route element={<HomeLayout />}>
+          <Route index element={<Landing />} />
+          <Route path="/blogs" element={<AllBlogsPage />} />
+          <Route path="/blog/:slug" element={<BlogPostPage />} />
+        </Route>
+
+        
+
+      
+
+
+     
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
